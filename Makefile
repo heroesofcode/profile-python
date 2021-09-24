ab9de1208d671888f3dd0344ba2257aed756d950
João Lucas <joaolucasfp2001@gmail.com>
2021-09-24 16:03:27 -0300

    Created unit test networking and add test in CI

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 setup: ## Dependencies
 	python -m pip install --upgrade pip
 	pip install setuptools wheel twine
+
+test: ## Run tests and
+	pip install pytest
+	pytest
+	pip install pytest-cov
+	pytest --cov=school_report
---
+++ tests/test_networking.py
@@ -0,0 +1,25 @@
+import unittest
+from profile_python.networking import Networking
+
+
+class TestNetworking(unittest.TestCase):
+
+    def test_request_success(self):
+        json = '["animal","career","celebrity","dev","explicit","fashion","food","history",' \
+               '"money","movie","music","political","religion","science","sport","travel"]'
+
+        url = "https://api.chucknorris.io/jokes/categories"
+        networking = Networking()
+        response = networking.request(url)
+        assert response == json
+
+    def test_passing_success(self):
+        url = "https://api.chucknorris.io/jokes/categories"
+        networking = Networking()
+        response = networking.request(url)
+
+        value = networking.parsing(response)
+        assert value[0] == "animal"
+
+if __name__ == '__main__':
+    unittest.main()
