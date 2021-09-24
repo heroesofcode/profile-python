8b206b672fc4dd7b84312537a4b41857bc169b1c
João Lucas <joaolucasfp2001@gmail.com>
2021-09-24 15:19:18 -0300

    Add options and add url for repos

--- profile_python/main.py
+++ profile_python/main.py
@@ -1,13 +1,35 @@
-import requests
-import json
 from profile_python.networking import Networking
 
 url = "https://api.github.com/users/joaolfp"
 url_repo = "https://api.github.com/users/joaolfp/repos"
 
 networking = Networking()
-response = networking.request(url_repo)
+response_datas = networking.request(url)
+response_repos = networking.request(url_repo)
 
-values = networking.parsing(response)
+values_datas = networking.parsing(response_datas)
+values_repos = networking.parsing(response_repos)
 
-print(values[0]['owner']['login'])
+def get_datas(datas):
+    print(datas['login'])
+    print(datas['name'])
+    print(datas['bio'])
+    print(datas['company'])
+    print(datas['blog'])
+    print(datas['location'])
+
+def get_repos(repos):
+    for repo in repos:
+        print(repo['name'])
+
+print("1 - My datas")
+print("2 - Repositories")
+
+print("-----------------------------------------------")
+
+option = input("Choose an option: ")
+
+if option == "1":
+    get_datas(values_datas)
+else:
+    get_repos(values_repos)
